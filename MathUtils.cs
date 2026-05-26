13be745031a6727acf1eff7eb6a05aa4cf105f07
hhy-clown <dedpepe77@gmail.com>
2026-05-26 10:25:02 +0300

    Добавлен класс MathUtils

---
+++ MathUtils.cs
@@ -0,0 +1,26 @@
+namespace CSharpLab
+{
+    class Program
+    {
+        static void Main()
+        {
+            MathUtils mathUtils;
+            global::System.Console.WriteLine(mathUtils.MathUtils());
+        }
+    }
+    public class MathUtils
+    {
+        public int x;
+        public int y;
+        public MathUtils(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public void MathUtils()
+        {
+            return x + y;
+        }
+    }
+}
+}
