3110e0ea42002ea7e443422624114912087617b8
hhy-clown <dedpepe77@gmail.com>
2026-05-26 09:51:43 +0300

    Update Helper.cs

--- Helper.cs
+++ Helper.cs
@@ -1,2 +1,14 @@
 namespace CSharpLab { class Program { static void Main() {
-System.Console.WriteLine("Start"); } } }
+  System.Console.WriteLine("Start"); } } 
+
+public class Krutoi
+{
+  public string name;
+  public string description;
+  public Krutoi(string name, string description)
+  {
+    this.name = name;
+    this.description = description;
+  }
+}
+                    }
